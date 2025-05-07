ce2e4a624c16ce776d162bdb5c811f6f074b47ca
Sibgat-Ul_Islam <sibgatulislam@gmail.com>
2025-05-07 10:04:46 +0600

    debug

--- engine/data/dataloader.py
+++ engine/data/dataloader.py
@@ -43,6 +43,9 @@ class DataLoader(data.DataLoader):
             format_string += "\n"
             format_string += "    {0}: {1}".format(n, getattr(self, n))
         format_string += "\n)"
+        print("=" * 6, "DEBUG", "=" * 6)
+        print(format_string)
+        print("=" * 6, "DEBUG", "=" * 6)
         return format_string
 
     def set_epoch(self, epoch):
